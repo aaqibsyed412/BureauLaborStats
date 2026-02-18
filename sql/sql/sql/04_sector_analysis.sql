7ceba8a13a51d391c0da9ff5cf296a6b3b7a540c
aaqibsyed412 <aaqibsyed4@gmail.com>
2026-02-18 01:52:37 -0500

    Update 04_sector_analysis.sql

--- sql/sql/sql/04_sector_analysis.sql
+++ sql/sql/sql/04_sector_analysis.sql
@@ -1,1 +1,27 @@
+-- Sector Employment Trends
+-- Compares employment levels across major sectors over time
+
+SELECT 
+    YEAR(date) AS year,
+    
+    CASE
+        WHEN VARIABLE_NAME ILIKE '%Manufacturing%' THEN 'Manufacturing'
+        WHEN VARIABLE_NAME ILIKE '%Retail%' THEN 'Retail'
+        WHEN VARIABLE_NAME ILIKE '%Construction%' THEN 'Construction'
+        WHEN VARIABLE_NAME ILIKE '%Health Care%' THEN 'Health Care'
+    END AS sector,
+    
+    AVG(value) AS avg_employment
+FROM SNOWFLAKE_PUBLIC_DATA_FREE.PUBLIC_DATA_FREE.BUREAU_OF_LABOR_STATISTICS_EMPLOYMENT_TIMESERIES
+WHERE 
+    VARIABLE_NAME ILIKE 'State and Metro Employment: All Employees%'
+    AND (
+        VARIABLE_NAME ILIKE '%Manufacturing%'
+        OR VARIABLE_NAME ILIKE '%Retail%'
+        OR VARIABLE_NAME ILIKE '%Construction%'
+        OR VARIABLE_NAME ILIKE '%Health Care%'
+    )
+GROUP BY year, sector
+HAVING sector IS NOT NULL
+ORDER BY year, sector;
 
