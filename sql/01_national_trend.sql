71a3842cdcdafd026efba659918f1f2939e7785c
aaqibsyed412 <aaqibsyed4@gmail.com>
2026-02-18 01:44:50 -0500

    Update 01_national_trend.sql

--- sql/01_national_trend.sql
+++ sql/01_national_trend.sql
@@ -1,1 +1,10 @@
+-- National Unemployment Trend (1976–2025)
+
+SELECT 
+    YEAR(date) AS year,
+    AVG(value) * 100 AS avg_unemployment_rate
+FROM SNOWFLAKE_PUBLIC_DATA_FREE.PUBLIC_DATA_FREE.BUREAU_OF_LABOR_STATISTICS_EMPLOYMENT_TIMESERIES
+WHERE VARIABLE_NAME = 'Local Area Unemployment: Unemployment Rate, Not seasonally adjusted, Monthly'
+GROUP BY year
+ORDER BY year;
 
